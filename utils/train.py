9c1aa70808313033918b4d499a301df4daa570d2
Prashant Tiwari <prashant.st8@gmail.com>
2023-11-28 03:54:25 +0530

    train loop updated

--- utils/train.py
+++ utils/train.py
@@ -4,9 +4,8 @@ from tqdm.notebook import tqdm
 def TrainLoopV1(
     model:torch.nn.Module,
     optimizer:torch.optim.Optimizer,
-    criterion:torch.nn.Module,
-    train_dataloader:torch.utils.data.DataLoader,
-    val_dataloader:torch.utils.data.DataLoader,
+    train_loader:torch.utils.data.DataLoader,
+    val_loader:torch.utils.data.DataLoader,
     scheduler=None,
     n_epochs:int=20,
     early_stopping_rounds:int=5,
@@ -20,28 +19,31 @@ def TrainLoopV1(
     best_weights = model.state_dict()
     for epoch in tqdm(range(n_epochs)):
         model.train()
-        print("Epoch {} | Learning Rate = {}".format(epoch, optimizer.param_groups[0]['lr']))
+        print("\n---------------------\nEpoch {} | Learning Rate = {}".format(epoch, optimizer.param_groups[0]['lr']))
         train_loss = 0
-        for i, image, mask in enumerate(train_dataloader):
-            image, mask = image.to(device), mask.to(device)
+        for i, images, target in enumerate(train_loader):
+            images = list(image.to(device) for image in images)
+            target = [{k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in t.items()} for t in target]
             optimizer.zero_grad()
-            outputs = model(image)
-            loss = criterion(outputs, mask)
-            train_loss += loss
-            loss.backward()
-            optimizer.step()
+            loss_dict = model(images, target)
+            losses = sum(loss for loss in loss_dict.values())
+            train_loss += losses
+            losses.backward()
+            optimizer.zero_grad()
             if i % batch_loss == 0:
-                print("Loss for batch {} = {}".format(i, loss))
-        print("\nTraining Loss for epoch {} = {}\n".format(epoch, train_loss))
+                print("Loss for Batch {} = {}".format(i, losses))
+
+        print("Loss for epoch {} = {}".format(epoch, train_loss))
 
         model.eval()
-        validation_loss = 0
-        with torch.inference_mode:
-            for image, mask in val_dataloader:
-                image, mask = image.to(device), mask.to(device)
-                outputs = model(image)
-                loss = criterion(outputs, mask)
-                validation_loss += loss
+        with torch.inference_mode():
+            validation_loss = 0
+            for images, target in val_loader:
+                images = list(image.to(device) for image in images)
+                target = [{k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in t.items()} for t in target]
+                loss_dict = model(images, target)
+                losses = sum(loss for loss in loss_dict.values())
+                validation_loss += losses
             
             if validation_loss < best_val_loss:
                 best_val_loss = validation_loss
