6d3a56d9fc067c068f24edbcb3622396caddfe67
Prashant Tiwari <prashant.st8@gmail.com>
2023-11-27 23:46:16 +0530

    instance segmentation dataset updated

--- utils/data.py
+++ utils/data.py
@@ -1,9 +1,11 @@
 import torch
 import cv2 as cv
-from PIL import Image
 import numpy as np
+from torchvision import tv_tensors
 from torch.utils.data import Dataset
-from torchvision.transforms.v2 import Compose, Resize, InterpolationMode, ToImage, ToDtype, Normalize
+from torchvision.io import read_image
+from torchvision.transforms import v2
+from torchvision.ops.boxes import masks_to_boxes
 
 def create_mask_from_json(json_data, image_size):
     """
@@ -41,11 +43,11 @@ def create_mask_from_json(json_data, image_size):
 
     return mask
 
-SegmentTransform = Compose([
-    ToImage(),
-    ToDtype(torch.float32, scale=True),
-    Resize((520,520), interpolation=InterpolationMode.BICUBIC, antialias=True),
-    Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+SegmentTransform = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Resize((520,520), interpolation=v2.InterpolationMode.BICUBIC, antialias=True),
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 class SemanticSegDataset(Dataset):
@@ -108,90 +110,88 @@ class SemanticSegDataset(Dataset):
         image = cv.cvtColor(cv.imread(self.images[index]), cv.COLOR_BGR2RGB)
         mask = torch.tensor(cv.imread(self.masks[index], cv.IMREAD_GRAYSCALE))
         mask = mask.unsqueeze(0)
-        mask = Resize((520, 520), interpolation=InterpolationMode.BICUBIC, antialias=True)(mask)
+        mask = v2.Resize((520, 520), interpolation=v2.InterpolationMode.BICUBIC, antialias=True)(mask)
         image = self.transform(image)
         return image, mask
 
 class InstanceSegDataset(Dataset):
     """
-    Custom dataset for instance segmentation tasks.
+    Custom dataset class for instance segmentation tasks.
 
     Args:
-        img_list (list): List of file paths for input images.
-        mask_list (list): List of file paths for corresponding masks.
+        img_list (list): List of file paths to input images.
+        mask_list (list): List of file paths to corresponding mask images.
+        train (bool, optional): Flag indicating whether the dataset is used for training.
+            Defaults to False.
 
     Attributes:
-        images (list): List of input image file paths.
-        masks (list): List of mask file paths corresponding to the images.
+        images (list): List of file paths to input images.
+        masks (list): List of file paths to corresponding mask images.
+        transform (torchvision.transforms.Compose): Composition of data transformations.
 
     Methods:
-        __len__(): Returns the number of images in the dataset.
-        __getitem__(index): Loads and processes the image and its corresponding mask at the given index.
-
-    Returns:
-        tuple: Tuple containing the processed image and a dictionary of target information.
-               The target dictionary contains "boxes", "labels", and "masks" for instance segmentation.
+        __len__(): Returns the total number of samples in the dataset.
+        __getitem__(index): Loads and preprocesses the image and mask at the specified index.
 
     Example:
-        dataset = InstanceSegDataset(img_list, mask_list)
-        img, target = dataset[0]
+        img_list = ["path/to/img1.jpg", "path/to/img2.jpg", ...]
+        mask_list = ["path/to/mask1.jpg", "path/to/mask2.jpg", ...]
+        dataset = InstanceSegDatasetv2(img_list, mask_list, train=True)
     """
 
-    def __init__(self, img_list, mask_list):
+    def __init__(self, img_list, mask_list, train: bool = False):
         """
-        Initializes the InstanceSegDataset with a list of image and mask file paths.
+        Initializes the InstanceSegDatasetv2 dataset.
 
         Args:
-            img_list (list): List of file paths for input images.
-            mask_list (list): List of file paths for corresponding masks.
+            img_list (list): List of file paths to input images.
+            mask_list (list): List of file paths to corresponding mask images.
+            train (bool, optional): Flag indicating whether the dataset is used for training.
+                Defaults to False.
         """
         self.images = img_list
         self.masks = mask_list
+        transforms = []
+        if train:
+            transforms.append(v2.RandomHorizontalFlip(0.5))
+        transforms.append(v2.ToDtype(torch.float, scale=True))
+        transforms.append(v2.ToPureTensor())
+        self.transform = v2.Compose(transforms)
 
     def __len__(self):
         """
-        Returns the number of images in the dataset.
+        Returns the total number of samples in the dataset.
 
         Returns:
-            int: Number of images in the dataset.
+            int: Number of samples in the dataset.
         """
         return len(self.images)
 
     def __getitem__(self, index):
         """
-        Loads and processes the image and its corresponding mask at the given index.
+        Loads and preprocesses the image and mask at the specified index.
 
         Args:
-            index (int): Index of the image in the dataset.
+            index (int): Index of the sample to retrieve.
 
         Returns:
-            tuple: Tuple containing the processed image and a dictionary of target information.
-                   The target dictionary contains "boxes", "labels", and "masks" for instance segmentation.
+            tuple: A tuple containing the preprocessed image and a dictionary of target information.
         """
-        img = Image.open(self.images[index]).convert("RGB")
-        mask = Image.open(self.masks[index])
-        mask = np.array(mask)
-        obj_ids = np.unique(mask)
+        img = read_image(self.images[index])
+        mask = read_image(self.masks[index])
+
+        obj_ids = torch.unique(mask)
         obj_ids = obj_ids[1:]
         num_objs = len(obj_ids)
-        masks = np.zeros((num_objs , mask.shape[0] , mask.shape[1]))
-        for i in range(num_objs):
-            masks[i][mask == i+1] = True
-        boxes = []
-        for i in range(num_objs):
-            pos = np.where(masks[i])
-            xmin = np.min(pos[1])
-            xmax = np.max(pos[1])
-            ymin = np.min(pos[0])
-            ymax = np.max(pos[0])
-            boxes.append([xmin , ymin , xmax , ymax])
-        boxes = torch.as_tensor(boxes , dtype = torch.float32)
-        labels = torch.ones((num_objs,) , dtype = torch.int64)
-        masks = torch.as_tensor(masks , dtype = torch.uint8)
-        
+        masks = (mask == obj_ids[:, None, None]).to(dtype=torch.uint8)
+        boxes = masks_to_boxes(masks)
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        img = tv_tensors.Image(img)
+
         target = {}
-        target["boxes"] = boxes
+        target["boxes"] = tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=v2.functional.get_size(img))
         target["labels"] = labels
-        target["masks"] = masks
-        img = ToImage()(img)
-        return ToDtype(torch.float32, scale=True)(img) , target
+        target["masks"] = tv_tensors.Mask(masks)
+
+        img, target = self.transform(img, target)
+        return img, target
