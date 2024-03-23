7a1536b63f563bc5642c60308a22a80da7042728
Prashant-Tiwari-Sign3 <prashant.tiwari@sign3labs.com>
2024-03-23 11:11:35 +0530

    jsons added

--- utils/data.py
+++ utils/data.py
@@ -12,20 +12,16 @@ def create_mask_from_json(json_data, image_size):
     Creates a binary mask based on polygonal regions specified in JSON data.
 
     Parameters:
-    - json_data (list): A list of dictionaries, each containing 'content' key
-                       representing a list of points forming a polygon.
-                       Each point is a dictionary with 'x' and 'y' keys.
+    - json_data (list): A list of dictionaries, each containing 'content' key representing a list of points forming a polygon. Each point is a dictionary with 'x' and 'y' keys.
     - image_size (tuple): A tuple representing the size of the target image (height, width).
 
     Returns:
-    - mask (numpy.ndarray): A binary mask with the specified image size,
-                           where regions enclosed by the polygons are set to 1,
-                           and the rest is set to 0.
+    - mask (numpy.ndarray): A binary mask with the specified image size, where regions enclosed by the polygons are set to 1, and the rest is set to 0.
 
     Example:
     ```
     json_data = [{'content': [{'x': 10, 'y': 20}, {'x': 30, 'y': 40}, ...]},
-                 {'content': [{'x': 50, 'y': 60}, {'x': 70, 'y': 80}, ...]}]
+                {'content': [{'x': 50, 'y': 60}, {'x': 70, 'y': 80}, ...]}]
     image_size = (100, 100)
     mask = create_mask_from_json(json_data, image_size)
     ```
@@ -153,7 +149,9 @@ class InstanceSegDataset(Dataset):
         self.masks = mask_list
         transforms = []
         if train:
-            transforms.append(v2.RandomHorizontalFlip(0.5))
+            transforms.append(v2.RandomHorizontalFlip(0.55))
+            transforms.append(v2.RandomVerticalFlip(0.55))
+            transforms.append(v2.RandomRotation(50))
         transforms.append(v2.ToDtype(torch.float, scale=True))
         transforms.append(v2.ToPureTensor())
         self.transform = v2.Compose(transforms)
